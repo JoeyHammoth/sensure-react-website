f73b33f66f30305952859e50e8bf527250b662ab
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-03-18 16:04:10 +1100

    left paragraph section

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import BluePanel from "./Panels/BluePanel.jsx";
-import YellowPanel from "./Panels/YellowPanel.jsx";
-import YellowPanelItem from "./Panels/YellowPinkPanel.jsx";
-import GreenButton from "./Buttons/Green.jsx";
-import GreyButton from "./Buttons/Grey.jsx";
-import BlackButton from "./Buttons/Black.jsx";
-import SlidePanel from "./Advanced/Slide.jsx";
+import LeftParagraph from "./Formats/LeftParagraph.jsx";
 
 const App = function () {
     return (
         <StrictMode>
             <div>
-                <h1>Placeholder</h1>
-                <BluePanel title="Hello" body="placeholder" />
-                <YellowPanel title="Hello" body="placeholder" />
-                <YellowPanelItem title="Hello" body="placeholder" />
-                <GreenButton text="Hello" />
-                <GreyButton text="Hello" />
-                <BlackButton text="Hello" />
-                <SlidePanel title="Hello" subtitle="placeholder" btn="Learn more" />
+                <LeftParagraph title="Welcome to Hell!!!" paragraph="lorem ipsum" alt="Image" img="/Images/img1.webp" />
             </div>
         </StrictMode>
     );
---
+++ src/Formats/LeftParagraph.jsx
@@ -0,0 +1,17 @@
+import "./LeftParagraph.css";
+
+const LeftParagraph = function ({ title, paragraph, img, alt }) {
+    return (
+        <div className="LeftParagraph">
+            <div className="left-section">
+                <div className="left-section-title">{title}</div>
+                <div className="left-section-paragraph">{paragraph}</div>
+            </div>
+            <div className="right-section">
+                <img src={img} alt={alt} />
+            </div>
+        </div>
+    )
+}
+
+export default LeftParagraph;
